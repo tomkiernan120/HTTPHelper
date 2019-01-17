226fdbe438cc6b69b4ffab93b56e0c75df2f528b
Tom <tkiernan120@gmail.com>
2019-01-17 23:16:28 +0000

    Update Tag

--- src/Tags/Tag.php
+++ src/Tags/Tag.php
@@ -5,64 +5,61 @@ namespace HTTPFriend\Tags;
 
 class Tag
 {
-    public $linkAttr = [
-      "href",
-      "codebase",
-      "cite",
-      "background",
-      "action",
-      "longdesc",
-      "src",
-      "usemap",
-      "classid",
-      "data",
-      "formaction",
-      "icon",
-      "manifest",
-      "poster",
-    ];
+  public $linkAttr = [
+    "href",
+    "codebase",
+    "cite",
+    "background",
+    "action",
+    "longdesc",
+    "src",
+    "usemap",
+    "classid",
+    "data",
+    "formaction",
+    "icon",
+    "manifest",
+    "poster",
+  ];
 
-    public function __construct( )
-    {
-        
-    }
+  public function __construct()
+  {
 
-    public function cleanURLs( $html, $urlreplace = null )
-    {
+  }
 
-      foreach ( $this->linkAttr as $attr ) {
-        $html = str_replace( "{$attr}=\"//", "{$attr}=\"##", $html );
+  public function cleanURLs($html, $urlreplace = null)
+  {
 
-        if( $urlreplace ){
-          $html = str_replace( "{$attr}=\"/", "{$attr}=\"{$urlreplace}", $html );
-        }
-        $html = str_replace( "{$attr}=\"##", "{$attr}=\"//", $html );
+    foreach ($this->linkAttr as $attr) {
+      $html = str_replace("{$attr}=\"//", "{$attr}=\"##", $html);
+
+      if ($urlreplace) {
+        $html = str_replace("{$attr}=\"/", "{$attr}=\"{$urlreplace}", $html);
       }
-
-      return $html;
+      $html = str_replace("{$attr}=\"##", "{$attr}=\"//", $html);
     }
 
-    public function createElement( $tag, $attr = [], $selfClosing = false, $close = false )
-    {
-      $element = "<" . (( $close ) ? "/" : "" );
-      $element .= "$tag";
-      if( !empty( $attr ) ){
-        foreach( $attr as $ak => $av ){
-          $element .= " {$ak}=\"{$av}\" ";
-        }
+    return $html;
+  }
+
+  public function createElement($tag, $attr = [], $selfClosing = false, $close = false)
+  {
+    $element = "<" . (($close) ? "/" : "");
+    $element .= "$tag";
+    if (!empty($attr)) {
+      foreach ($attr as $ak => $av) {
+        $element .= " {$ak}=\"{$av}\" ";
       }
-
-
-      $element .= (( $selfClosing ) ? "/" : "" ) . ">";
-
-      return $element;
     }
+    $element .= (($selfClosing) ? "/" : "") . ">";
+    return $element;
+  }
 
-    public function output( $options = [] )
-    {
-      ob_start();
-      echo $this->createElement( $options["tag"], $options["attr"], $options['selfClosing'], $options["close"] );
-      echo ob_get_clean();
-    }
+  public function output($options = [])
+  {
+    ob_start();
+    echo $this->createElement($options["tag"], $options["attr"], $options['selfClosing'], $options["close"]);
+    echo ob_get_clean();
+  }
 
 }
